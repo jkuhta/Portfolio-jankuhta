e52e7c7cae70ebea58611a5b7957c4c8ef90ac70
jkuhta <jan.kuhta@gmail.com>
2023-10-08 22:30:03 +0200

    add interests component

---
+++ src/components/pages/interests/Data.js
@@ -0,0 +1,58 @@
+import Image1 from "../../../assets/interest.jpeg";
+import Image2 from "../../../assets/interest-action.jpeg";
+
+export const interestItems = [
+  {
+    id: 1,
+    title: "Photography",
+    icon: "uil uil-camera",
+    subItems: [
+      {
+        image: Image1,
+        subtitle: "Drone Photography",
+      },
+      {
+        image: Image2,
+        subtitle: "Action Photography",
+      },
+      {
+        image: Image1,
+        subtitle: "DSLR Photography",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Sports",
+    icon: "uil uil-basketball",
+    subItems: [
+      {
+        image: Image1,
+        subtitle: "Kitesurfing",
+      },
+      {
+        image: Image2,
+        subtitle: "Snowboarding",
+      },
+      {
+        image: Image1,
+        subtitle: "Basketball",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Music",
+    icon: "uil uil-music",
+    subItems: [
+      {
+        image: Image1,
+        subtitle: "Piano",
+      },
+      {
+        image: Image2,
+        subtitle: "Guitar",
+      },
+    ],
+  },
+];
--- src/components/pages/interests/Interests.js
+++ src/components/pages/interests/Interests.js
@@ -1,12 +1,65 @@
 import React from "react";
+import "./Interests.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import { interestItems } from "./Data";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const Interests = () => {
   return (
     <>
-      <section className="interests section" id="interests">
+      <section className="interest section" id="interests">
         <h1 className="section__title">Interests</h1>
         <span className="section__subtitle">What I do in my free time</span>
-        <div className="interests__container container grid"></div>
+        <div className="interest__container container grid">
+          {interestItems.map((item) => (
+            <div className="interest__content" key={item.id}>
+              <h3 className="interest__title">
+                {" "}
+                <i className={`${item.icon} interest__icon`}></i>
+                {item.title}
+              </h3>
+              <div className="interest__box">
+                <div>
+                  <Swiper
+                    slidesPerView={1}
+                    spaceBetween={30}
+                    loop={true}
+                    pagination={{
+                      clickable: true,
+                    }}
+                    navigation={true}
+                    modules={[Pagination, Navigation]}
+                    className="interest__slider"
+                  >
+                    {item.subItems.map((subItem, subIndex) => {
+                      return (
+                        <SwiperSlide key={subIndex}>
+                          <figure className="interest__swiper-pic-warp">
+                            <img
+                              className="interest__swiper-img"
+                              src={subItem.image}
+                              alt=""
+                            />
+                          </figure>
+                          <div className="interest__subtitle">
+                            <h5 className="interest__subtitle-text">
+                              {subItem.subtitle}
+                            </h5>
+                          </div>
+                        </SwiperSlide>
+                      );
+                    })}
+                  </Swiper>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </section>
     </>
   );
