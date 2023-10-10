ab50e1b9edbe21277279f3bfdccf25395f05e677
jkuhta <jan.kuhta@gmail.com>
2023-10-10 12:09:31 +0200

    tba update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import "./App.css";
 // import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/header/Header";
-import Home from "./components/pages/home/Home";
-import About from "./components/pages/about/About";
-import Skills from "./components/pages/skills/Skills";
-import Services from "./components/pages/services/Services";
-import Qualification from "./components/pages/qualification/Qualification";
-import Interests from "./components/pages/interests/Interests";
+// import Home from "./components/pages/home/Home";
+// import About from "./components/pages/about/About";
+// import Skills from "./components/pages/skills/Skills";
+// import Services from "./components/pages/services/Services";
+// import Qualification from "./components/pages/qualification/Qualification";
+// import Interests from "./components/pages/interests/Interests";
+// import Testimonials from "./components/pages/testimonials/Testimonials";
+// import Contact from "./components/pages/contact/Contact";
+// import Projects from "./components/pages/projects/Projects";
 import Footer from "./components/footer/Footer";
 import Scrollup from "./components/scrollup/Scrollup";
-import Testimonials from "./components/pages/testimonials/Testimonials";
-import Contact from "./components/pages/contact/Contact";
-import Projects from "./components/pages/projects/Projects";
 import Tba from "./components/pages/tba/Tba";
 
 function App() {
