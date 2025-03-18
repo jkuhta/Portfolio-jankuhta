0314cd2bcb4b60edafe1330a8a706674efac7f05
Jan Kuhta <jan.kuhta@gmail.com>
2025-03-18 21:50:57 +0100

    add updated data

--- src/components/pages/home/Data.js
+++ src/components/pages/home/Data.js
@@ -2,22 +2,22 @@ import React from "react";
 import "./Home.css";
 
 const Data = () => {
-  return (
-    <>
-      <div className="home__data">
-        <h1 className="home__title">Jan Kuhta</h1>
-        <h3 className="home__subtitle">Full - Stack Developer</h3>
-        <p className="home__description">
-          I'm an enthusiastic Full - Stack Developer at Medius.si, holding a
-          bachelor's degree in Computer Science, driven by a passion for
-          learning and soon embarking on a master's journey.
-        </p>
-        <a href="#contact" className="button button--flex">
-          Contact Me <i className="uil uil-message contact-icon"></i>
-        </a>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="home__data">
+                <h1 className="home__title">Jan Kuhta</h1>
+                <h3 className="home__subtitle">Software Developer</h3>
+                <p className="home__description">
+                    I'm a Software developer at Medius.si with a bachelor's degree in Computer Science.
+                    I enjoy learning new things and building useful projects.
+                    Soon, I'm starting my master's to keep growing both personally and professionally.
+                </p>
+                <a href="#contact" className="button button--flex">
+                    Contact Me <i className="uil uil-message contact-icon"></i>
+                </a>
+            </div>
+        </>
+    );
 };
 
 export default Data;
