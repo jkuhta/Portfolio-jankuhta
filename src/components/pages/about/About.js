ace7dc9545f870f25aff28e1f274a6c89cb67145
Jan Kuhta <jan.kuhta@gmail.com>
2025-03-18 21:50:57 +0100

    add updated data

--- src/components/pages/about/About.js
+++ src/components/pages/about/About.js
@@ -4,34 +4,35 @@ import CV from "../../../assets/jan-kuhta_CV_ENG.pdf";
 import Info from "./Info";
 import "./About.css";
 
-function About({ toggleTab }) {
-  return (
-    <>
-      <section className="about section" id="about">
-        <h2 className="section__title">About Me</h2>
-        <span className="section__subtitle">My Introduction</span>
-        <div className="about__container container grid">
-          <img src={AboutImg} alt="" className="about__img" />
-          <div className="about__data">
-            <Info toggleTab={toggleTab} />
-            <p className="about__description">
-              As a Computer Science graduate with a diverse skill set, I'm eager
-              to explore fields of Data Science, Computer Vision and Web
-              Development. Coming from a QA Engineering background, I bring a
-              sharp eye for detail and a commitment to quality. I'm enthusiastic
-              about expanding my knowledge in these fields.
-            </p>
-            <div className="button__cv">
-              <a download="" href={CV} className="button button--flex">
-                Download CV
-                <i className="uil uil-file-download-alt button__cv-icon"></i>
-              </a>
-            </div>
-          </div>
-        </div>
-      </section>
-    </>
-  );
+function About({toggleTab}) {
+    return (
+        <>
+            <section className="about section" id="about">
+                <h2 className="section__title">About Me</h2>
+                <span className="section__subtitle">My Introduction</span>
+                <div className="about__container container grid">
+                    <img src={AboutImg} alt="" className="about__img"/>
+                    <div className="about__data">
+                        <Info toggleTab={toggleTab}/>
+                        <p className="about__description">
+                            I'm a Computer Science graduate working as a Full-Stack Developer at Medius.si. I enjoy
+                            building web applications and solving problems, with experience in both frontend and backend
+                            development and a strong eye for detail from my QA Engineering background. <br/>
+                            I’m especially interested in Data Science, Computer Vision, and Web Development, and I’m
+                            always looking for ways to learn and grow. Next, I’ll be starting my master’s studies to
+                            dive deeper into these fields and take on new challenges.
+                        </p>
+                        <div className="button__cv">
+                            <a download="" href={CV} className="button button--flex">
+                                Download CV
+                                <i className="uil uil-file-download-alt button__cv-icon"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
 }
 
 export default About;
--- src/components/pages/about/Info.js
+++ src/components/pages/about/Info.js
@@ -1,57 +1,57 @@
 import React from "react";
 
-const Info = ({ toggleTab }) => {
+const Info = ({toggleTab}) => {
 
-  const birthDate = new Date(2000, 6, 17);
-  const today = new Date();
-  const age = today.getFullYear() - birthDate.getFullYear() -
-      (today < new Date(today.getFullYear(), birthDate.getMonth(), birthDate.getDate()) ? 1 : 0);
+    const birthDate = new Date(2000, 6, 17);
+    const today = new Date();
+    const age = today.getFullYear() - birthDate.getFullYear() -
+        (today < new Date(today.getFullYear(), birthDate.getMonth(), birthDate.getDate()) ? 1 : 0);
 
-  return (
-    <>
-      <div className="about__info grid">
-        <div className="about__box">
-          <i className="uil uil-user-circle about__icon"></i>
-          <h3 className="about__title">Age</h3>
-          <span className="about__subtitle">{age} years old</span>
-        </div>
-        <div className="about__box">
-          <i className="uil uil-location-point about__icon"></i>
-          <h3 className="about__title">Country</h3>
-          <span className="about__subtitle">Slovenia</span>
-        </div>
-        <div className="about__box">
-          <i className="uil uil-globe about__icon"></i>
-          <h3 className="about__title">Languages</h3>
-          <span className="about__subtitle">SLO, ENG, GER, CRO</span>
-        </div>
+    return (
+        <>
+            <div className="about__info grid">
+                <div className="about__box">
+                    <i className="uil uil-user-circle about__icon"></i>
+                    <h3 className="about__title">Age</h3>
+                    <span className="about__subtitle">{age} years old</span>
+                </div>
+                <div className="about__box">
+                    <i className="uil uil-location-point about__icon"></i>
+                    <h3 className="about__title">Country</h3>
+                    <span className="about__subtitle">Slovenia</span>
+                </div>
+                <div className="about__box">
+                    <i className="uil uil-globe about__icon"></i>
+                    <h3 className="about__title">Languages</h3>
+                    <span className="about__subtitle">SLO, ENG, GER, CRO</span>
+                </div>
 
-        <a
-          href="#qualification"
-          className="about__box "
-          onClick={() => toggleTab(2)}
-        >
-          <i className="uil uil-briefcase-alt about__icon"></i>
-          <h3 className="about__title">Experience</h3>
-          <span className="about__subtitle">2 Years Working</span>
-        </a>
-        <a
-          href="#qualification"
-          className="about__box"
-          onClick={() => toggleTab(1)}
-        >
-          <i className="uil uil-medal about__icon"></i>
-          <h3 className="about__title">Education</h3>
-          <span className="about__subtitle">Bachlor's degree</span>
-        </a>
-        <a href="#projects" className="about__box">
-          <i className="uil uil-check-circle about__icon"></i>
-          <h3 className="about__title">Completed</h3>
-          <span className="about__subtitle">9+ projects</span>
-        </a>
-      </div>
-    </>
-  );
+                <a
+                    href="#qualification"
+                    className="about__box "
+                    onClick={() => toggleTab(2)}
+                >
+                    <i className="uil uil-briefcase-alt about__icon"></i>
+                    <h3 className="about__title">Experience</h3>
+                    <span className="about__subtitle">2 Years Working</span>
+                </a>
+                <a
+                    href="#qualification"
+                    className="about__box"
+                    onClick={() => toggleTab(1)}
+                >
+                    <i className="uil uil-medal about__icon"></i>
+                    <h3 className="about__title">Education</h3>
+                    <span className="about__subtitle">Bachlor's degree</span>
+                </a>
+                <a href="#projects" className="about__box">
+                    <i className="uil uil-check-circle about__icon"></i>
+                    <h3 className="about__title">Completed</h3>
+                    <span className="about__subtitle">12+ projects</span>
+                </a>
+            </div>
+        </>
+    );
 };
 
 export default Info;
